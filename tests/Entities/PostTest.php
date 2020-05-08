7b5c08719ef9dfb275de3063f1efbe82a0ff87dd
Elvis Henrique Pereira <elvishp2006@gmail.com>
2020-05-08 19:45:00 -0300

    test: fix tests for entities

--- tests/Entities/PostTest.php
+++ tests/Entities/PostTest.php
@@ -6,18 +6,20 @@ use WPSteak\Entities\Post as Entity;
 
 final class PostTest extends \PHPUnit\Framework\TestCase {
 
-	/** @var \WPSteak\Entities\Post&\PHPUnit\Framework\MockObject\MockObject $repository */
+	/** @var \WPSteak\Entities\Post&\PHPUnit\Framework\MockObject\MockObject $entity */
 	private Entity $entity;
 
+	private \WP_Post $post;
+
 	public function setUp(): void {
+		$this->post = $this->getMockBuilder( \WP_Post::class )->getMock();
 		$this->entity = $this->getMockBuilder( Entity::class )
-			->disableOriginalConstructor()
-			->setMethods( ['get_post'] )
-			->getMock();
+			->setConstructorArgs( [$this->post] )
+			->getMockForAbstractClass();
 	}
 
 	public function test_get_post(): void {
-		$this->assertInstanceOf( '\WP_Post', $this->entity->get_post() );
+		$this->assertSame( $this->post, $this->entity->get_post() );
 	}
 
 }
--- tests/Entities/TermTest.php
+++ tests/Entities/TermTest.php
@@ -6,18 +6,20 @@ use WPSteak\Entities\Term as Entity;
 
 final class TermTest extends \PHPUnit\Framework\TestCase {
 
-	/** @var \WPSteak\Entities\Term&\PHPUnit\Framework\MockObject\MockObject $repository */
+	/** @var \WPSteak\Entities\Term&\PHPUnit\Framework\MockObject\MockObject $entity */
 	private Entity $entity;
 
+	private \WP_Term $term;
+
 	public function setUp(): void {
+		$this->term = $this->getMockBuilder( \WP_Term::class )->getMock();
 		$this->entity = $this->getMockBuilder( Entity::class )
-			->disableOriginalConstructor()
-			->setMethods( ['get_term'] )
-			->getMock();
+			->setConstructorArgs( [$this->term] )
+			->getMockForAbstractClass();
 	}
 
 	public function test_get_term(): void {
-		$this->assertInstanceOf( '\WP_Term', $this->entity->get_term() );
+		$this->assertSame( $this->term, $this->entity->get_term() );
 	}
 
 }
